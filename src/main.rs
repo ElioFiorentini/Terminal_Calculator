63c45eddde2d68d5237e3900bae9b26993e9cb8f
Elio Fiorentini <elio.fiorentini1@proton.me>
2024-11-11 22:15:49 +0100

    Project outline defined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+/*  Autor: Elio Fiorentini
+    Creation date: 11/11/2024
+    Last modification: 11/11/2024
+
+    Objective:
+    Building a fully fonctional and usable console calculator.
+    It must take a string in input and output
+    the result of the operation entered.
+    It must be able to compute math functions
+    such as sin, power, square root, etc...
+*/
+
+enum Token {
+    Operand(f64),
+    Operator(String),
+    Function(String),
+}
+
+fn infix_to_postfix(input: String) -> Vec<Token> {
+    // Implementation of the "Shunting yard algorithm" from Edsger Dijkstra.
+    // Seen at: https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+    Vec::new()
+}
+
+fn compute(postfix: Vec<Token>) -> f64 {
+    0.0
+}
+
 fn main() {
-    println!("Hello, world!");
+    // User input: to do
+
+    let user_input = String::from("15+4/2*8");
+    let postfix_notation = infix_to_postfix(user_input);
+    let result = compute(postfix_notation);
+
+    println!("{result}");
 }
