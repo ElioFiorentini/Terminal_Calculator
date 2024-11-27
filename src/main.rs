e35f53b1b3476172dd0b3e682146282aa741204d
Elio Fiorentini <elio.fiorentini1@proton.me>
2024-11-27 10:59:21 +0100

    Implementation of the Shunting Yard algorithm, work in progress, still not working.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,16 @@
 */
 
 use core::f64;
+use std::collections::VecDeque;
 use std::io;
 use std::io::Write;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Token {
     Operand(f64),
     Operator(char),
     Function(String),
+    Ponctuation(char),
     None,
 }
 
@@ -48,8 +50,6 @@ fn slice_to_token(input_string: &str) -> Token {
             "÷" => Token::Operator('/'),
             "%" => Token::Operator('%'),
             "mod" => Token::Operator('%'),
-            "(" => Token::Operator('('),
-            ")" => Token::Operator(')'),
 
             // Functions:
             // power
@@ -87,6 +87,9 @@ fn slice_to_token(input_string: &str) -> Token {
             // Others:
             "pi" => Token::Operand(f64::consts::PI),
             "π" => Token::Operand(f64::consts::PI),
+            "," => Token::Ponctuation(','),
+            "(" => Token::Ponctuation('('),
+            ")" => Token::Ponctuation(')'),
             _ => Token::None,
         }
     }
@@ -145,10 +148,158 @@ fn string_to_token(input_string: String) -> Vec<Token> {
     token_vector
 }
 
-fn infix_to_postfix(infix: Vec<Token>) -> Vec<Token> {
+fn is_parenthesis(o: Token) -> bool {
+    if o == Token::Ponctuation('(') || o == Token::Ponctuation(')') {
+        true
+    } else {
+        false
+    }
+}
+
+fn is_exponential(o: Token) -> bool {
+    if o == Token::Function("exp".to_string()) {
+        true
+    } else {
+        false
+    }
+}
+
+fn is_mult_or_div(o: Token) -> bool {
+    if o == Token::Operator('*') || o == Token::Operator('/') || o == Token::Operator('%') {
+        true
+    } else {
+        false
+    }
+}
+
+fn is_add_or_sub(o: Token) -> bool {
+    if o == Token::Operator('+') || o == Token::Operator('-') {
+        true
+    } else {
+        false
+    }
+}
+
+fn takes_precedence(o1: Token, o2: Token) -> bool {
+    if o1 == o2 {
+        return false;
+    }
+    if is_parenthesis(o1) {
+        if is_parenthesis(o2) {
+            return false;
+        }
+        return true;
+    }
+    if is_exponential(o1) {
+        if is_parenthesis(o2) || is_exponential(o2) {
+            return false;
+        }
+        return true;
+    }
+    if is_mult_or_div(o1) {
+        if is_parenthesis(o2) || is_exponential(o2) || is_mult_or_div(o2) {
+            return false;
+        }
+        return true;
+    }
+    if is_add_or_sub(o1) {
+        if is_parenthesis(o2) || is_exponential(o2) || is_mult_or_div(o2) || is_add_or_sub(o2) {
+            return false;
+        }
+        return true;
+    }
+    false
+}
+
+fn same_precedence(o1: Token, o2: Token) -> bool {
+    if (is_parenthesis(o1) && is_parenthesis(o2))
+        || (is_exponential(o1) && is_exponential(o2))
+        || (is_mult_or_div(o1) && is_mult_or_div(o2))
+        || (is_add_or_sub(o1) && is_add_or_sub(o2))
+    {
+        true
+    } else {
+        false
+    }
+}
+
+fn is_left_associative(o: Token) -> bool {
+    if o == Token::Function("pow".to_string()) {
+        false
+    } else {
+        true
+    }
+}
+
+fn is_function(f: Token) -> bool {
+    match f {
+        Token::Function(_) => true,
+        _ => false,
+    }
+}
+
+fn infix_to_postfix(infix: Vec<Token>) -> Result<Vec<Token>, &'static str> {
     // Implementation of the "Shunting yard algorithm" from Edsger Dijkstra.
     // Seen at: https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 
+    let mut output_queue: VecDeque<Token> = VecDeque::new();
+    let mut operator_stack: Vec<Token> = Vec::new();
+
+    for token in infix.iter() {
+        match token {
+            Token::Operand(n) => output_queue.push_back(Token::Operand(*n)),
+            Token::Function(f) => operator_stack.push(Token::Function(*f)),
+            Token::Operator(o1) => {
+                let o2 = match operator_stack.last() {
+                    Some(o2) => o2.clone(),
+                    None => Token::None,
+                };
+                while match o2 {
+                    Token::None => false,
+                    _ => true,
+                } && (takes_precedence(o2, Token::Operator(*o1))
+                    || (same_precedence(Token::Operator(*o1), o2)
+                        && is_left_associative(Token::Operator(*o1))))
+                {
+                    output_queue.push_back(operator_stack.pop().unwrap());
+                }
+                operator_stack.push(Token::Operator(*o1));
+            }
+            Token::Ponctuation(p) => {
+                if *p == ',' {
+                    while *operator_stack.last().unwrap() != Token::Ponctuation('(') {
+                        output_queue.push_back(operator_stack.pop().unwrap());
+                    }
+                } else if *p == '(' {
+                    operator_stack.push(Token::Operator(*p));
+                } else if *p == ')' {
+                    while !operator_stack.is_empty()
+                        && *operator_stack.last().unwrap() != Token::Ponctuation('(')
+                    {
+                        output_queue.push_back(operator_stack.pop().unwrap());
+                    }
+                    if operator_stack.is_empty()
+                        || *operator_stack.last().unwrap() != Token::Ponctuation('(')
+                    {
+                        return Err("Error: unmatching parenthesis.");
+                    }
+                    let _ = operator_stack.pop();
+                    if is_function(*operator_stack.last().unwrap()) {
+                        output_queue.push_back(operator_stack.pop().unwrap());
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+    while !operator_stack.is_empty() {
+        if is_parenthesis(*operator_stack.last().unwrap()) {
+            return Err("Error: unmatching parenthesis.");
+        }
+        output_queue.push_back(operator_stack.pop().unwrap());
+    }
+    println!("{:?}", output_queue);
+    Ok(output_queue.into())
 }
 
 fn compute(postfix: Vec<Token>) -> f64 {
@@ -182,7 +333,13 @@ fn main() {
     */
 
     // let user_input = String::from("15+4/2*8");
-    let postfix_notation = infix_to_postfix(string_to_token(user_input));
+    let postfix_notation: Vec<Token> = match infix_to_postfix(string_to_token(user_input)) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{}", e);
+            Vec::new()
+        }
+    };
     let result = compute(postfix_notation);
 
     println!("{result}");
